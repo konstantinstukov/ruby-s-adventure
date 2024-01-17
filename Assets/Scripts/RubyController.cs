f486bc54b93db2b611d21dcbaef06d746f9f37b6
Konstantin Stukov <k1xsks@gmail.com>
2024-01-17 23:13:44 +0300

    World Interactions - Collectibles

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -1,10 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 
 public class RubyController : MonoBehaviour
 {
+    [SerializeField] int maxHealth = 5;
+    public int MaxHealth
+    {
+        get { return maxHealth; }
+    }
+    [SerializeField] float speed = 3.0f;
+    int currentHealth;
+    public int CurrentHealth
+    {
+        get { return currentHealth; }
+    }
+
     Rigidbody2D rb;
     float horizontalInput;
     float verticalInput;
@@ -13,6 +26,7 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -22,11 +36,18 @@ public class RubyController : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         Vector2 position = transform.position;
-        position.x += 3f * horizontalInput * Time.deltaTime;
-        position.y += 3f * verticalInput * Time.deltaTime;
+        position.x += speed * horizontalInput * Time.deltaTime;
+        position.y += speed * verticalInput * Time.deltaTime;
 
         rb.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
