f32313d96175627e3764d2511739cf1167cfb07e
Konstantin Stukov <k1xsks@gmail.com>
2024-01-17 22:01:40 +0300

    world interactions - blocking movement

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -1,25 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rb;
+    float horizontalInput;
+    float verticalInput;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
 
+    void FixedUpdate() {
+        Vector2 position = transform.position;
         position.x += 3f * horizontalInput * Time.deltaTime;
         position.y += 3f * verticalInput * Time.deltaTime;
 
-        transform.position = position;
+        rb.MovePosition(position);
     }
 }
